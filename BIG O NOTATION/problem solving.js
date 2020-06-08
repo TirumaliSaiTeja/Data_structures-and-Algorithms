7595900baa5ad12ca1bc538ce30815fbd248aa8d
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-08 18:33:33 +0530

    character count using ascii

--- BIG O NOTATION/problem solving.js
+++ BIG O NOTATION/problem solving.js
@@ -1,8 +1,8 @@
-function charCount(str){
+/*function charCount(str){
     var solve = {};
     
     for(i=0; i<str.length; i++){
-        var char = str[i];
+        var char = str[i].toLowerCase();
         if(solve[char]>0){
             solve[char]++;
         }
@@ -11,4 +11,32 @@ function charCount(str){
         }
     }
     return solve;
+}*/
+
+// character count in another way
+
+function charCount(str){
+    var result = {};
+    for(var char of str){
+        char = char.toLowerCase();
+        if(isAlphaNumeric(char)){
+            result[char] = ++result[char]||1;
+        }
+    }
+
+    return result;
+}
+
+
+function isAlphaNumeric(char){
+    var code = char.charCodeAt(0);
+    if(
+        !(code>47&&code<58)&&
+        !(code>64&&code<91)&&
+        !(code>96&&code<123)
+    )
+    {
+        return false;
+    }
+    return true;
 }
