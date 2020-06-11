ce8a025620ce374a86766f816695892830972745
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-11 21:44:01 +0530

    anagram callenge

--- Problem-solving-patterns/anagram task.js
+++ Problem-solving-patterns/anagram task.js
@@ -1,16 +1,26 @@
 function validAnagram(str1, str2) {
+    //here we check length pf two sords or strings
     if (str1.length !== str2.length) {
+        //if length is not equal, we return false and we exit the loop
       return false;
     }
+
+    //here we store values in a variable
   
     const value = {};
+
+    // here we itterate over first string
   
     for (let i = 0; i < str1.length; i++) {
       let letter = str1[i];
-      // if letter exists, increment, otherwise set to 1
+
+      //if the letter present it will increment or other wise it will set to 1
+      
       value[letter] ? value[letter] += 1 : value[letter] = 1;
     }
     console.log(value)
+
+    //here we itterate over second string
   
     for (let i = 0; i < str2.length; i++) {
       let letter = str2[i];
@@ -25,5 +35,4 @@ function validAnagram(str1, str2) {
     return true;
   }
   
-  // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
-  validAnagram('anagrams', 'nagaramm')
+  
