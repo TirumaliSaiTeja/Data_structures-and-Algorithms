09a1777ac5a977ff09507c58732f5d32c3db1404
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-11 22:18:00 +0530

    multiple pointers - Refactored approach

--- Problem-solving-patterns/Multiple pointers.js
+++ Problem-solving-patterns/Multiple pointers.js
@@ -15,3 +15,30 @@ function sumZero(arr){
                     }
     }
 }
+
+// Refactored Approach
+//Time complexity- o(n)
+//space complexity- o(1)
+
+function sumZero(arr){
+
+    //We define position of variables
+    let left = 0;
+    let right = arr.length -1;
+    while(left<right){
+        //We are declaring the sum of two arrays
+        let sum = arr[left] + arr[right];
+        //if the sum is equal to zero then we return a pair of values
+        if(sum===0){
+            return [arr[left], arr[right]];
+        }
+        //if sum is greater than zero, then the array of pointers from right side will reduce by 1
+        else if (sum>0){
+            right--;
+        }
+        else{
+            //if the above condition fails, here it array of pointers from left side will increament
+            left++;
+        }
+    }
+}
