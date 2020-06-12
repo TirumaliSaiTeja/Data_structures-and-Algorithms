d650004c4aa5aebdc52f1b40072d60d90fededa0
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-12 22:40:42 +0530

    sliding window- Naive Approach

--- Problem-solving-patterns/countUnique.js
+++ Problem-solving-patterns/countUnique.js
@@ -1,21 +1,22 @@
-function countUnique(arr){
+function countUnique(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
-    if(arr.length === 0){
-        return 0;
+  // we set initial value of i to 0
+  var i = 0;
+
+  // we itterate j in the array
+  for (let j = 1; j < arr.length; j++) {
+    // if i is not equal to j, then we increment i
+    if (arr[i] !== arr[j]) {
+      //we increment value of i
+      i++;
+      //we eill move position of i
+
+      arr[i] = arr[j];
     }
+  }
 
-    // we set initial value of i to 0
-    var i = 0;
-
-    // we itterate j in the array
-    for(let j=1; j<arr.length; j++){
-        // if i is not equal to j, then we increment i
-        if(arr[i]!==arr[j]){
-            i++;
-
-            arr[i]=arr[j]
-        }
-    }
-
-    return i+1;
+  return i + 1;
 }
