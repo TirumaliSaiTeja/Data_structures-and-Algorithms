a82fd79c9f9eb052b3ca8bfb79848d7dff6258d3
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-13 21:52:29 +0530

    updated 1

--- Problem-solving-patterns/sliding window.js
+++ Problem-solving-patterns/sliding window.js
@@ -1,11 +1,16 @@
 //naive approach
+//time complexity - o(n^2)
 
 function maxSubarraySum(arr, num) {
+  //here we take array and number
   if (num > arr.length) {
+    //if the number is greater than array length it returns null
     return null;
   }
   var max = -Infinity;
+  //here we take infinity because it will result in array of negative values
   for (let i = 0; i < arr.length - num + 1; i++) {
+    //here we itterate the loop
     temp = 0;
     for (let j = 0; j < num; j++) {
       temp += arr[i + j];
