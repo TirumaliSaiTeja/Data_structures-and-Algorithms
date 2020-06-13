eb5763554341914c706a13cfac0190ddc59dd419
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-13 22:21:02 +0530

    divide and conquer

---
+++ Problem-solving-patterns/divide and conquer.js
@@ -0,0 +1,11 @@
+//Linear Approach
+// time complexity- o(n)
+
+function search(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
