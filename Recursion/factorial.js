c810aa67a4559bbfffe4ceb09809155275113e73
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-16 12:19:06 +0530

    itterative and recursive for factorial

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,7 +1,16 @@
+// Factorial Itterative
+
 function factorial(num) {
   let total = 1;
   for (let i = num; i > 1; i--) {
     total = total * i;
   }
   return total;
+}
+
+// factorial recursively
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
 }
