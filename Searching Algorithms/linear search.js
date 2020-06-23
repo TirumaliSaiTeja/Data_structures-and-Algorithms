51a13308044cba40073122b4b3e26071042c9fd4
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-23 23:53:53 +0530

    linear search

---
+++ Searching Algorithms/linear search.js
@@ -0,0 +1,16 @@
+// Javascript has search
+// there are many different search methods on arrays in javascript
+// indexOf, includes, find, findIndex
+
+// Linear search
+
+// it will start checking the entire array
+
+function Search(arr, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == val) {
+      return i
+    }
+  }
+  return -1
+}
