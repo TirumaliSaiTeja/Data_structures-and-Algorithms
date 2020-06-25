2984b27af677729417aa9f9ad7e160f80edc95bd
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-06-25 23:12:06 +0530

    binary search 2

--- Searching Algorithms/binary search.js
+++ Searching Algorithms/binary search.js
@@ -7,13 +7,40 @@
 //simplified version
 
 function Binary(arr, val) {
+  //in function we are taking a array and a value
+  //defining the start, end and middle of the array
   var start = 0
   var end = arr.length - 1
   var middle = Math.floor((start + end) / 2)
+  //checking the condition, when middle value of the array is not
+  //equal to value and start value is less than equal to end of the array
   while (arr[middle] !== val && start <= end) {
     if (val < arr[middle]) end = middle - 1
     else start = middle + 1
     middle = Math.floor((start + end) / 2)
   }
+  //here we are using turnary operator
+  //it will return only when middle is equal to value
+  //then returns middle and if not it will return -1
   return arr[middle] === val ? middle : -1
+}
+
+//another version
+function Search(arr, val) {
+  var start = 0
+  var end = arr.length - 1
+  var middle = Math.floor((start + end) / 2)
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) {
+      end = middle - 1
+    } else {
+      start = middle + 1
+    }
+    middle = Math.floor((start + end) / 2)
+
+    if (arr[middle] === val) {
+      return middle
+    }
+    return -1
+  }
 }
