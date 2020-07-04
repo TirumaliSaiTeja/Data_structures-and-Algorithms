b26f5de65e74ddc65c6466c17f4ad94d0d3cb279
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-07-04 23:27:30 +0530

    minimum sub array length

---
+++ Big o- Challenges/sliding window 2.js
@@ -0,0 +1,13 @@
+//minSubArraylen
+
+//write a function called mi sub array length
+//which accepts two parameters- an array of positive integers and a positive integer
+
+//This function should return the minimal length of a contiguous subarray of which the sum
+//is greater than or equal to the integer passed to the function
+//if htere isn;t one, returm 0 instead
+
+//Examples
+
+//minSubArrayLen([2,3,1,2,4,3],7)-2, [4,3] is the smallest subarray
+//minSubArrayLen([2,1,6,5,4],9)-2, [5,4] is the smallest subarray
