09353e1376fbb3bf82aa11b6606d9be721d630d2
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-07-10 23:46:48 +0530

    comparision between BS, IS, SS

---
+++ Insertion sort/comparision.js
@@ -0,0 +1,13 @@
+// algorithm      time complexity      time complexity     time complexity      space complexity
+//                    best                 avg                 worst
+// Bubble sort           o(n)             o(n^2)                     o(n^2)             o(1)
+// Insertion sort        o(n)             o(n^2)                     o(n^2)             o(1)
+// Selection sort       o(n^2)           o(n^2)                     o(n^2)             o(1)
+
+// 0verview
+
+// Sorting is fundamental
+// Bubble sort, selection sort and insertion sort are all
+// roughly equivalent
+// All have average time complexities that are quadratic
+// We can do better but we need more complex algorithms
