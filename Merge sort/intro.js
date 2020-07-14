4824222dab6de787bd13769d3265dca0beacf459
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-07-14 16:24:18 +0530

    implementation of merge sort

--- Merge sort/intro.js
+++ Merge sort/intro.js
@@ -19,3 +19,13 @@
 // Step 4: [2,3,4,5,7,9] [13,18,20,23,34,43]
 // here we sort the array
 // Step 5: [2,3,4,5,7,9,13,18,20,23,34,43]
+
+// Merging arrays
+
+// In order to implement merge sort, its useful to first implement
+// a function responsible for merging two sorted arrays
+// Given two arrays which are sorted, this helper function should create a new
+// array which is also sorted, and consists of all of the elements
+// in the two input arrays
+// This function should run in o(n+m) time and o(n+m) space and
+// should not modify the parameters passed to it.
